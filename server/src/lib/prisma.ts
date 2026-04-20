4ca0e555c14541d94aee2dd86db9f8bd3032579f
AkshayPP-3 <24061100617@reva.edu.in>
2026-04-20 14:53:04 +0530

    channged export to lib and generated prisma

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,13 +1,10 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-const { PrismaClient } = require("@prisma/client") as {
-	PrismaClient: new (...args: any[]) => any;
-};
+import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
-
 
 export { prisma };
