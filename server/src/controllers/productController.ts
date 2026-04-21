464e809d365d391e03d0271e2750a2cdd18f2b09
AkshayPP-3 <24061100617@reva.edu.in>
2026-04-21 21:41:00 +0530

    Add getProducts controller to fetch all products from Prisma

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -1,8 +1,18 @@
-import { prisma } from "../exports/prisma"
+import { prisma } from "../lib/prisma.js";
 import type { Request, Response } from "express";
 
 
-export const getProducts = async (_: Request, res: Response) => {};
+export const getProducts = async(_:Request, res:Response)=>{
+    try{
+        const products = await prisma.product.findMany({
+            orderBy:{id: "asc"}
+        });
+        res.status(200).json(products)
+    }
+    catch{
+        res.status(500).json({message:"Internal server error"})
+    }
+}
 export const getProduct = async (_: Request, res: Response) => {};
 export const createProduct = async (_: Request, res: Response) => {};
 export const updateProduct = async (_: Request, res: Response) => {};
