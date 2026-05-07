82d05cd2239b0682aa4ceb436685fbeda437289d
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-07 13:02:04 +0530

    Fix Redis TLS config

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -1,19 +1,8 @@
 import Redis from "ioredis";
 
 export const redis = new Redis(process.env.REDIS_URL as string, {
-  tls: true,
+  tls: {},
   maxRetriesPerRequest: null,
-  retryStrategy: (times) => {
-    const delay = Math.min(times * 50, 2000);
-    return delay;
-  },
-  reconnectOnError: (err) => {
-    const targetError = "READONLY";
-    if (err.message.includes(targetError)) {
-      return true;
-    }
-    return false;
-  },
 });
 
 redis.on("connect", () => {
